ddd01e0401b10e9a517e13f5b09b4e6fcf51f2d6
mihaitufescu <mihai.tufescu@gmail.com>
2023-05-10 09:29:36 +0300

    v2

--- linkBin/WebApp/urls.py
+++ linkBin/WebApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.PageRoute, name="index")
+    path('index/', views.PageRoute, name="index"),
+    path('', views.PageRoute, name="index")
 ]
